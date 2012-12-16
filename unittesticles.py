6042f1f026f0dd90473e540b09ea7d386958256c
L Gabriel Miller <gmiller2007@gmail.com>
2012-12-16 13:38:22 -0500

    Updated tests

--- unittesticles.py
+++ unittesticles.py
@@ -158,7 +158,8 @@ class SpiderTests(unittest.TestCase):
         input4 = {'site1':[         'site2', 'site3', 'site4'],
                   'site2':[                  'site3', 'site4'],
                   'site3':['site1', 'site2'         ],
-                  'site4':[                         ]}
+                  'site4':[                         ],
+                  'site5':[                         ]}
 
         a_random_number = randint(0,1000)
         self.assertEqual(page_rank(outgoing_links_to_pagerank(input3), a_random_number), page_rank(outgoing_links_to_pagerank(input4), a_random_number)), "Unscanned site pagerank is incorrect"
